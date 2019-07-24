656fe9035e8f56c3b05f92479f2df13859f09faa
guodaxia395962201 <395962201@qq.com>
2019-07-24 16:54:57 +0800

    第七次提交

--- js/dior_home.js
+++ js/dior_home.js
@@ -1,2 +1,23 @@
-
-  
+  var backToTop=document.querySelector(".backToTop");
+  var clientHeight=document.documentElement.clientHeight||document.body.clientHeight;
+  var isTop=true;
+  var timer=null;
+  backToTop.style.display="none";
+  window.onscroll=function(){
+    var topH=document.documentElement.scrollTop||document.body.scrollTop; 
+    if(topH>0){
+      backToTop.style.display="block";
+    }else{
+        backToTop.style.display="none";
+    }
+  }
+  backToTop.onclick=function(){
+    timer=setInterval(function(){
+        var topH=document.documentElement.scrollTop||document.body.scrollTop;
+        var stepLength=Math.ceil(topH/5);
+        document.documentElement.scrollTop=document.body.scrollTop=topH-stepLength;
+        if(topH==0){
+          clearInterval(timer);
+        }
+     },50);
+  }
