99643aa8e51f5192835321be21939c1848977abd
ChrisChoubaba <709346613@qq.com>
2019-07-25 09:01:16 +0800

    第八次提交

--- js/buy.js
+++ js/buy.js
@@ -11,3 +11,30 @@
 			        $('.buy-content').eq(index).addClass('buy-show').siblings().removeClass('buy-show');
 			    })
 			});	
+			$(function (){
+			    if (localStorage.getItem('wgoods')) {
+			        var codeArr = JSON.parse( localStorage.getItem('wgoods') ).code;
+			
+			        if (codeArr.length == 0) {
+			            return false;
+			        }
+			        $.ajax({
+			            url: '../data/w-goods.json',
+			            type: 'get',
+			            cache: false,
+			            dataType: 'json',
+			            success: function (data){
+			                var buyresults = '';
+			                $.each(codeArr,function (i,item){
+			                    $.each(data,function (index,obj){
+			                        if (item == obj.code) {
+			                            buyresults += `<li><a href="#"><img src="${obj.imgurl}" alt="" /></a></li>`;
+			                        }
+			                    });
+			                });
+			                $('.recentlylist').html(buyresults);
+			            }
+			        });
+			    }
+			})
+
